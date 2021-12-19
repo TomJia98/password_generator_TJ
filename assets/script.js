9de8e4407b37268bb0d1accb55cb6a251ae09ed4
TomJia98 <Tomchristian1998@gmail.com>
2021-12-19 13:19:46 +0800

    removed <,>,= from characters list to fix bug where them appearing cut the password off

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ const listOfNumbers = [1,2,3,4,5,6,7,8,9,0]
 //contains 10 items
 const listOfLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 //contains 26 items
-const listOfSpecialCharacters = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~"]
+const listOfSpecialCharacters = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","?","@","[","]","^","_","{","|","}","~"]
 //contains 27 items
 const listOfUppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 //contains 26 items
@@ -35,43 +35,26 @@ generatePassword.addEventListener("click", function(event){
     for (var i = 0; i < passwordLength; i++) {
         x = Math.floor(Math.random() * 4); //generates a number between 0 and 3
         if (x == 0 && hasLowercase) {
-            const selectedLowercase = listOfLowercase[Math.floor(Math.random() * 26)]; //sets selectedLowercase to a random lowercase letter
+            const selectedLowercase = listOfLowercase[Math.floor(Math.random() * listOfLowercase.length)]; //sets selectedLowercase to a random lowercase letter
             finishedPassword.push(selectedLowercase);
         }
          else if (x == 1 && hasUppercase) {
-            const selectedUppercase = listOfUppercase[Math.floor(Math.random() * 26)];
+            const selectedUppercase = listOfUppercase[Math.floor(Math.random() * listOfUppercase.length)];
             finishedPassword.push(selectedUppercase);
         }
         else if (x == 2 && hasNumbers) {
-            const selectedNumber = listOfNumbers[Math.floor(Math.random() * 10)];
+            const selectedNumber = listOfNumbers[Math.floor(Math.random() * listOfNumbers.length)];
             finishedPassword.push(selectedNumber);
         }
         else if (x == 3 && hasSpecial) {
-            const selectedSpecial = listOfSpecialCharacters[Math.floor(Math.random() * 27)];
+            const selectedSpecial = listOfSpecialCharacters[Math.floor(Math.random() * listOfSpecialCharacters.length)];
             finishedPassword.push(selectedSpecial);
         } else {i--}
     
-    }} else {alert("Your password needs something in it!")}
+    }} else {alert("Your password needs something in it!")}//alerts user if nothing is selected (otherwise it crashs)
 
-var filteredPassword = finishedPassword.filter(function(value, index, arr){
-    return value == ",";
-})
+
 
     document.getElementById("password").innerHTML = finishedPassword.join('');
-
-  
-
-
-
-    // console.log(hasLowercase);
-    // console.log(hasUppercase);
-    // console.log(hasNumbers);
-    // console.log(hasSpecial);
-    // console.log(passwordLength);
-    // console.log(listOfUppercase);
-
-    // grabs the data from checkboxes and slider and logs it when you press the button
-
-
 
 })
